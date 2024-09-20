9c019d503c207a5b02bbfee01332b3a5ce6cab79
hallma886 <hallma886@northwested.org>
2024-09-20 13:43:57 -0400

    Add files via upload

---
+++ concat_mc.py
@@ -0,0 +1,13 @@
+# Task 1
+first_name = 'Matthew'
+fav_game = 'Five Nights at Freddys'
+print("Hello my name is "+ first_name +" and my favorite game is "+ fav_game +".")
+print()
+# Task 2
+print("What I have learned about concatenation:")
+print("One thing is that you cant use numbers like words!")
+print("Another thing is that you have to set it up like String + variable + string")
+print()
+# Task 3
+item_price = 5.29
+print("A double whopper at Burger King costs " + "$" + str(item_price) + ".")
